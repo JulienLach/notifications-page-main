7c7599af7327672f398474ae6677a5362b4a6d56
JulienLach <leadedchamps@gmail.com>
2023-11-29 15:49:00 +0100

    ajout exercice JS ok

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const counter = document.getElementById('counter');
 
 
+
     function updateCounter() {
         const activeNotifications = document.querySelectorAll('.notification.active').length;
         counter.textContent = activeNotifications;
     }
+
+    // Set the first three notifications to the active state by default
+    for (let i = 0; i < 3 && i < notifications.length; i++) {
+        notifications[i].classList.add('active');
+        redDots[i].classList.add('active');
+    }
+
+    // Update the counter after setting the default active notifications
+    updateCounter();
 
     notifications.forEach((notification, index) => {
         notification.addEventListener('click', () => {
