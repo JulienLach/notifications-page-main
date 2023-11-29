4f64ecb6d40bc65070e2feba80c65232826dccd6
JulienLach <leadedchamps@gmail.com>
2023-11-29 15:44:05 +0100

    exercice JS ok

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const notifications = document.querySelectorAll('.notification');
     const redDots = document.querySelectorAll('.red-dot');
     const markAll = document.getElementById('mark-all');
+    const counter = document.getElementById('counter');
 
+
+    function updateCounter() {
+        const activeNotifications = document.querySelectorAll('.notification.active').length;
+        counter.textContent = activeNotifications;
+    }
 
     notifications.forEach((notification, index) => {
         notification.addEventListener('click', () => {
             notification.classList.toggle('active');
             redDots[index].classList.toggle('active');
+            updateCounter();
         });
     });
 
@@ -15,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         notifications.forEach((notification, index) => {
             notification.classList.remove('active');
             redDots[index].classList.remove('active');
+            updateCounter();
         });
     });
 
